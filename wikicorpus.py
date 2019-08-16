04acb21ade6ba3bf16f5977d9b40179c98ed244d
Harang Ju <wngkfkd94@gmail.com>
2019-08-15 23:34:27 -0400

    wikicorpus module

---
+++ wikicorpus.py
@@ -0,0 +1,27 @@
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.utils import simple_preprocess
+import sys
+from wikidump import WikiDump
+
+class WikiCorpus:
+    def __init__(self, path_xml, path_index):
+        self.dump = WikiDump(path_xml, path_index)
+        self.names = list(self.dump.idx.keys())
+    
+    def __iter__(self):
+        self.i = 0
+        return self
+    
+    def __next__(self):
+        if self.i < len(self.names):
+            sys.stdout.write("\rCorpus index: " + str(self.i) + 
+                             '/' + str(len(self.names)))
+            sys.stdout.flush()
+            self.i += 1
+            return TaggedDocument(self.doc_at(self.i), [self.i])
+        else:
+            raise StopIteration
+    
+    def doc_at(self, index):
+        doc = self.dump.load_page(self.names[index])
+        return simple_preprocess(doc.strip_code())
