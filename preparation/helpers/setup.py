e836509874d67d4cd5fdddf58e43316ed1731619
Harang Ju <harangju@gmail.com>
2020-05-19 02:20:35 -0400

    update cython setup.py

--- preparation/helpers/setup.py
+++ preparation/helpers/setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
-    ext_modules = cythonize("sparse_manhattan.pyx")
+    ext_modules = cythonize("sparse_manhattan.pyx"),
+    include_dirs = [numpy.get_include()]
 )
