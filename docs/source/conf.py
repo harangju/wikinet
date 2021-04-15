297865fa895e659df4fea3fe31d34673c46cc4d3
Harang Ju <harangju@icloud.com>
2021-04-15 16:30:18 -0400

    update version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Harang Ju'
 author = 'Harang Ju'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
